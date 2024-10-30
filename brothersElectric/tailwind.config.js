781984e236fe851ad8a1615eb1655e9e8f6d2195
Jonathon Nolden <jnolden3927@gmail.com>
2024-10-30 14:20:40 -0700

    initial setup
    added npm react-app, tailwind, preline and react router dom

---
+++ brothersElectric/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  content: [
+    // './src/**/*.{html,js}',
+      'node_modules/preline/dist/*.js',
+  ],
+  plugins: [
+    // require('@tailwindcss/forms'),
+      require('preline/plugin'),
+  ],
+}
